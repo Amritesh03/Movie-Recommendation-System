6f0f212c2d190291e2afba0c6f0beea26b0915a1
Amritesh Kumar Dwivedi <dwivediamritesh37@gmail.com>
2025-09-08 21:02:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import requests
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+def recommend(movie_title, movies_df, similarity):
+    movie_index = movies_df[movies_df['title'] == movie_title].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for i in movie_list:
+        movie_id = movies_df.iloc[i[0]].movie_id
+
+        recommended_movies.append(movies_df.iloc[i[0]].title)
+        # fetch poster from API
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
+
+# Load data
+movies_df = pickle.load(open('movies.pkl','rb'))
+movies_list = movies_df['title'].values
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+# Streamlit UI
+st.title('Movie Recommendation System')
+
+selected_movie = st.selectbox(
+    "Select a movie:",
+    movies_list,
+)
+
+st.write("You selected:", selected_movie)
+
+if st.button("Recommend"):
+    names, posters = recommend(selected_movie, movies_df, similarity)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
